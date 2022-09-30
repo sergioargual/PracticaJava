cfc636bf9c4ee74200dad7e9722b154a3c0228c3
sga_1 <sga_1@DESKTOP-MNDRFQG>
2022-09-30 21:24:04 +0200

    Version 1.0.2

--- Libros/src/Main.java
+++ Libros/src/Main.java
@@ -1,10 +1,26 @@
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import org.json.*;
-public class Main 
+import java.time.LocalDate;
+public class Main implements Archivos//implementamos la interface
 {
+	
+	public Path Salida(String nombreArchivo, String Texto) throws IOException
+	{
+		Path file1 = Paths.get(nombreArchivo);				
+		List<String> lineas1 = Arrays.asList(Texto);
+		return Files.write(file1, lineas1, StandardCharsets.UTF_8);
+	}
+	
 	static ArrayList<Usuario> listaUsuarios = new ArrayList<>();
 	public static void main(String[] args) 
 	{
@@ -26,6 +42,8 @@ public class Main
 			U.add(new Usuario("Sergio", i));
 		}
 		
+		
+
 		try
 		{
 			URL url = new URL("https://www.el-tiempo.net/api/json/v2/provincias/28");
@@ -60,27 +78,42 @@ public class Main
 				listaTiempo.add(new Tiempo(((JSONObject) iter).getString("name"), ((JSONObject) iter).getJSONObject("stateSky").getString("description"), Integer.parseInt(((JSONObject) iter).getJSONObject("temperatures").getString("max")), Integer.parseInt(((JSONObject) iter).getJSONObject("temperatures").getString("min"))));
 				// sobre cada elemento del JSONArray, estamos creando un JSONObject -> ((JSONObject) iter) <-
 			}
-
+			ArrayList<String> texto1 = new ArrayList<>();
+			Main obj = new Main(); //creamos un objeto de la clase donde hayamos implementado la interface
 			for (Tiempo iter2:listaTiempo)
 			{
 				
 				if(iter2.getTemperatura_max() >= 24)
 				{
-					System.out.println(iter2+"Gradiente: "+(iter2.getTemperatura_max()-iter2.getTemperatura_min())+"\n"+"En "+iter2.getNombre()+" hace "+"Calor" +"\n");
+					texto1.add(iter2+"Gradiente: "+(iter2.getTemperatura_max()-iter2.getTemperatura_min())+"\n"+"En "+iter2.getNombre()+" hace "+"Calor" +"\n");
 				}
 				else
 				{
-					System.out.println(iter2+"Gradiente: "+(iter2.getTemperatura_max()-iter2.getTemperatura_min())+"\n"+"En "+iter2.getNombre()+" hace "+"Frío"+"\n");
+					texto1.add(iter2+"Gradiente: "+(iter2.getTemperatura_max()-iter2.getTemperatura_min())+"\n"+"En "+iter2.getNombre()+" hace "+"Frío"+"\n");
 				}
+					for (int i=0;i<texto1.size();i++)
+					{
+						obj.Salida("Salida_"+iter2.getNombre()+"_"+LocalDate.now()+".txt", texto1.get(i));
+					}
 				
 			}
-
+			
+			
 			
 		}
+		
+		
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
 	}
 
+
 }
+
+interface Archivos
+{
+	public Path Salida(String nombreArchivo, String Texto) throws IOException;
+}
+
